8f23e648896e094f81cbc56eb93f90f907603082
James Chambers <james@jameschambers.com>
2013-11-04 11:56:23 -0500

    Added sample projects

---
+++ SignalRShapes/SignalRShapes/Shapes/ShapeManagerHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRShapes.Shapes
+{
+    public class ShapeManagerHub : Hub
+    {
+
+        public void ShapeMoved(int x, int y)
+        {
+            Clients.Others.moveShape(x, y);
+            
+        }
+
+        public void Reset()
+        {
+            Clients.All.moveShape(0, 0);
+        }
+
+    }
+}
---
+++ SimpleSignal/SimpleSignal/Hello.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SimpleSignal
+{
+    public class HelloHub : Hub
+    {
+        public void Hello(string message)
+        {
+            Clients.All.hello(message);
+        }
+    }
+}
